9a8f7c0a3987341943fdd35baadcf413f90b1441
Quitomos <paul_marquez@dlsu.edu.ph>
2025-03-27 20:04:27 +0800

    adwd

--- src/server.js
+++ src/server.js
@@ -5,7 +5,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-
+const urlParams = new URLSearchParams(window.location.search);
+const eventId = urlParams.get('eventId');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
